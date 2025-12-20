d02a2f2ab5cba9edf4faced17b1eebabe2b0b426
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2025-12-20 11:20:34 +0530

    Time: 8 ms (74.40%) | Memory: 47.4 MB (7.68%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -1,11 +1,14 @@
 class Solution {
-    public int minDeletionSize(String[] str) {
-        int n = str.length;
-        int m = str[0].length();
-        int res =0 ;
-        for(int i = 0 ; i < m ; ++i){
-            for(int j = 1 ; j < n; ++j){
-                if(str[j].charAt(i) < str[j-1].charAt(i)){
+    public int minDeletionSize(String[] strs) {
+        int m = strs.length, res= 0;
+        char[][] matrix = new char[m][];
+        for (int i = 0; i < m; i++) {
+            matrix[i] = strs[i].toCharArray();
+        }
+        int n = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < matrix.length - 1; j++) {
+                if (matrix[j][i] > matrix[j + 1][i]) {
                     ++res;
                     break;
                 }
