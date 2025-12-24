3e3499faf89ef456440b9023820e497f5d2f602f
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2025-12-24 12:03:21 +0530

    Time: 2 ms (12.57%) | Memory: 47.5 MB (7.95%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int sp = image[sr][sc] ;
-        Queue<int[]> q = new ArrayDeque<>();
+        int[][] stack = new int[2500][];
+        int tos = -1;
         image[sr][sc] = color;
-        q.offer(new int[]{sr,sc});
+        stack[++tos] = new int[]{sr,sc};
         int m = image.length;
         int n = image[0].length;
         boolean[][] vis = new boolean[m][n];
         int[][] dir = {{0,-1},{0,1},{1,0},{-1,0}};
-        while(!q.isEmpty()){
-            int[] u = q.poll();
+        while(tos != -1){
+            int[] u = stack[tos--];
             vis[u[0]][u[1]] = true;
             for(int[] d:dir){   
                 int dx = u[0] + d[0] , dy = u[1] + d[1];
                 if(dx >= 0 && dx < m && dy >= 0 && dy < n && !vis[dx][dy] && image[dx][dy] == sp){
                     image[dx][dy] = color;
                     vis[dx][dy] = true;
-                    q.offer(new int[]{dx,dy});
+                    stack[++tos] = new int[]{dx,dy};
                 }
             }
         }
