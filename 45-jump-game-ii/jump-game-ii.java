eace13d1fcffbd0becffc20dca9573d29c0b2ba8
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2025-12-28 20:44:41 +0530

    Time: 1 ms (99.13%) | Memory: 47.6 MB (6.39%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int end = 0;
+        int res = 0;
+        int farthest = 0;
+        for(int i = 0 ; i < nums.length - 1 ; ++i){
+            farthest = Math.max(farthest,i+nums[i]);
+            if(farthest >= nums.length - 1){
+                ++res;
+                break;
+            }
+            if(i == end){
+                ++res;
+                end = farthest;
+            }
+        }
+        return res;
+    }
+}
