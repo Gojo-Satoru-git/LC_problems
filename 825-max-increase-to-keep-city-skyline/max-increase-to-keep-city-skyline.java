772135fa5ae64d9f5c157f1d2c91fd274bfdde10
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-01-17 12:29:01 +0530

    Time: 1 ms (58.91%) | Memory: 45.5 MB (67.82%) - LeetSync

---
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int res =0;
+        int n = grid.length;
+        int[] maxC = new int[n];
+        int[] maxR = new int[n];
+        for(int i = 0 ; i < n ; ++i ){
+            for(int  j  = 0 ; j < n ; ++j){
+                maxR[i] = Math.max(maxR[i],grid[i][j]);
+                maxC[j] = Math.max(maxC[j],grid[i][j]);
+            }
+        }
+        for(int i = 0 ; i < n ; ++i ){
+            for(int  j  = 0 ; j < n ; ++j){
+               int canBeIncreased = Math.min(maxR[i],maxC[j]);
+               res += canBeIncreased > grid[i][j] ? (canBeIncreased - grid[i][j]) : 0;
+            }
+        }
+        return res;
+        
+    }
+}
