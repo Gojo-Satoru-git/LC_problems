1d7f5489733b1cd5881f6a45ad6c873ec06abf3c
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-01-18 11:58:51 +0530

    Time: 5 ms (97.06%) | Memory: 46.7 MB (20.59%) - LeetSync

--- 1311-largest-magic-square/largest-magic-square.java
+++ 1311-largest-magic-square/largest-magic-square.java
@@ -1,37 +1,57 @@
 class Solution {
-    private boolean isValidMS(int[][] sqr,int i,int j,int k){
-        int[] rSum = new int[k];
-        int[] cSum = new int[k];
-        int diaSum = 0;
-        int antiDiaSum = 0;
-        for(int x = i ; x < i + k ; ++x){
-            for(int y = j ; y <j + k ; ++y){
-                rSum[x-i] += sqr[x][y];
-                cSum[y-j] += sqr[x][y];
-                if(x-i == y-j) diaSum += sqr[x][y];
-                if(x-i + y - j == k - 1) antiDiaSum += sqr[x][y];
+    public int largestMagicSquare(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        long[][] rowPrefix = new long[n][m + 1];
+        long[][] colPrefix = new long[n + 1][m];  
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                rowPrefix[i][j + 1] = rowPrefix[i][j] + grid[i][j];
+            }
+        }   
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                colPrefix[i + 1][j] = colPrefix[i][j] + grid[i][j];
             }
         }
-        int sum = diaSum;
-        for(int x = 0 ; x < k ;++x){
-            if(rSum[x] != sum)return false;
-            if(cSum[x] != sum)return false;
-        }
-        if(sum != antiDiaSum) return false;
-        return true;
-    }
-    public int largestMagicSquare(int[][] grid) {
-        int res = 0 ;
-        int m = grid.length;
-        int n = grid[0].length;
-        int k = Math.min(m,n);
-        for(int x = k ;  x >= 2 ; --x ){
-            for(int i  = 0 ; i + x - 1 < m ; ++i){
-                for(int j = 0 ; j + x - 1 < n ; ++j){
-                    if(isValidMS(grid,i,j,x))return x;
+        for (int k = Math.min(n, m); k > 1; k--) {
+            for (int r = 0; r <= n - k; r++) {
+                for (int c = 0; c <= m - k; c++) {
+                    
+                    long target = rowPrefix[r][c + k] - rowPrefix[r][c];
+                    boolean isMagic = true;
+                    
+                    for (int i = 0; i < k; i++) {
+                        long currRowSum = rowPrefix[r + i][c + k] - rowPrefix[r + i][c];
+                        if (currRowSum != target) {
+                            isMagic = false;
+                            break;
+                        }
+                    }
+                    if (!isMagic) continue;
+                
+                    for (int j = 0; j < k; j++) {
+                        long currColSum = colPrefix[r + k][c + j] - colPrefix[r][c + j];
+                        if (currColSum != target) {
+                            isMagic = false;
+                            break;
+                        }
+                    }
+                    if (!isMagic) continue;
+                    
+                    long d1 = 0, d2 = 0;
+                    for (int i = 0; i < k; i++) {
+                        d1 += grid[r + i][c + i];
+                        d2 += grid[r + i][c + k - 1 - i];
+                    }
+                    
+                    if (d1 == target && d2 == target) {
+                        return k;
+                    }
                 }
             }
         }
+        
         return 1;
     }
 }
