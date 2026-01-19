40af791c4bb796770c04ce0978decc70abe32320
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-01-19 13:42:58 +0530

    Time: 17 ms (18.78%) | Memory: 57.3 MB (79.19%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,29 +1,29 @@
 class Solution {
     public int maxSideLength(int[][] mat, int threshold) {
-        int m  = mat.length;
-        int n = mat[0].length;
-        long[][] rowPreSum  = new long[m][n+1];
-        int res = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                rowPreSum[i][j + 1] = rowPreSum[i][j] + mat[i][j];
+        int m = mat.length, n = mat[0].length;
+        int[][] pre = new int[m+1][n+1];
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                pre[i][j] = mat[i-1][j-1] 
+                          + pre[i-1][j] 
+                          + pre[i][j-1] 
+                          - pre[i-1][j-1];
             }
         }
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j){
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 int low = 1, high = Math.min(m - i, n - j);
-                while(low <= high) {
+                while (low <= high) {
                     int mid = low + (high - low) / 2;
-                    int sum = 0;
-                    for (int l = i; l < i + mid; ++l) {
-                        sum += rowPreSum[l][j + mid] - rowPreSum[l][j];
-                    }
+                    int x2 = i + mid, y2 = j + mid;
+                    int sum = pre[x2][y2] - pre[i][y2] - pre[x2][j] + pre[i][j];
                     if (sum <= threshold) {
                         res = Math.max(res, mid);
                         low = mid + 1;
                     } else {
-                        high = mid - 1;           
+                        high = mid - 1;
                     }
                 }
             }
