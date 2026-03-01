e6a1bbd02e17215ae5ac17d3d9942fc64ef0ad8a
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-03-01 10:26:37 +0530

    Time: 15 ms (82.55%) | Memory: 47 MB (30.97%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,29 @@
 class Solution {
-    private int f(int i, int t, int[] coins , int[][] dp){
-        if(i == coins.length)return Integer.MAX_VALUE/2;
-        if(t == 0) return 0;
-        if(dp[i][t] != -1)return dp[i][t];
-        int take = Integer.MAX_VALUE/2;
-        if(t - coins[i] >= 0){
-           take = 1 + f(i,t-coins[i],coins,dp); 
-        }
-        int ntake = f(i+1,t,coins,dp);
-        return dp[i][t] = Math.min(take,ntake);
-    }
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length ;
-        int[][] dp = new int[n][amount+1];
-        for(int i = 0; i < n ; ++i){
-            Arrays.fill(dp[i],-1);
+        int n = coins.length;
+        int INF = Integer.MAX_VALUE / 2;
+        int[][] dp = new int[n][amount + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
         }
-        int res = f(0,amount,coins,dp);
-        return res == Integer.MAX_VALUE/2?-1:res;
+        for (int a = 1; a <= amount; a++) {
+            if (a % coins[0] == 0) {
+                dp[0][a] = a / coins[0];
+            } else {
+                dp[0][a] = INF;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int a = 1; a <= amount; a++) {
+                int take = INF;
+                if (a - coins[i] >= 0) {
+                    take = 1 + dp[i][a - coins[i]];
+                }
+                int ntake = dp[i - 1][a];
+                dp[i][a] = Math.min(take, ntake);
+            }
+        }
+
+        return dp[n - 1][amount] >= INF ? -1 : dp[n - 1][amount];
     }
 }
