2e1efed789793bf992cdd7c144001d2fac9910c6
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-03-05 16:18:22 +0530

    Time: 5 ms (53.53%) | Memory: 43.9 MB (29.95%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -5,11 +5,6 @@ class Solution {
             if(i%2 == 0 && s.charAt(i) == '1')++ct1;
             else if(i % 2 == 1 && s.charAt(i) == '0')++ct1;
         }
-        int ct2 = 0;
-        for(int i = 0 ; i < s.length() ; ++i){
-            if(i%2 == 0 && s.charAt(i) == '0')++ct2;
-            else if(i % 2 == 1 && s.charAt(i) == '1')++ct2;
-        }  
-        return Math.min(ct1,ct2);
+        return Math.min(ct1,s.length() - ct1);
     }
 }
