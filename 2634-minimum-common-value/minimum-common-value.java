98b9300571e31d27d2da31b92e01b726cd28a39a
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-05-19 21:45:09 +0530

    Time: 1 ms (98.36%) | Memory: 80.3 MB (63.48%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -2,6 +2,9 @@ class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int i = 0 ,  j = 0 ;
         int n = nums1.length , m = nums2.length;
+        if (nums1[n - 1] < nums2[0] || nums2[m - 1] < nums1[0]) {
+            return -1;
+        }
         while(i < n && j < m){
             while(i < n && nums1[i] < nums2[j])++i;
             while(j < m && i < n && nums2[j] < nums1[i])++j;
