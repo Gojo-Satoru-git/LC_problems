7f6c59efaf1185a59131e5e39b8c376e7bf978da
PRAVEEN KUMAR C <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-06-07 22:07:39 +0530

    Time: 2 ms (37.22%) | Memory: 46.8 MB (96.54%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,14 +1,14 @@
 class Solution {
-    private void dfs(int i,int[][] graph,int[] vis){
-        Stack<Integer> st = new Stack<>();
-        st.push(i);
+    private void dfs(int i,int[][] graph,int[] vis,int[] stack){
+        int tos = -1;
+        stack[++tos] = i;
         vis[i] = 0;
-        while(!st.isEmpty()){
-            int u = st.pop();
+        while(tos != -1){
+            int u = stack[tos--];
             for(int v:graph[u]){
                 if(vis[v] == -1){
                     vis[v] = vis[u] == 0 ? 1 : 0;
-                    st.push(v);
+                    stack[++tos] = v;
                 }
             }
         }
@@ -16,10 +16,11 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length ;
         int[] vis = new int[n];
+        int[] stack = new int[n];
         Arrays.fill(vis,-1);
         for(int i = 0 ; i < n ; i++){
             if(vis[i] == -1){
-                dfs(i,graph,vis);
+                dfs(i,graph,vis,stack);
             }
         }
         //System.out.println(Arrays.toString(vis));
