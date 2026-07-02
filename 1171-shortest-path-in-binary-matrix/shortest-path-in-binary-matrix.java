e828351c1afd9477f10c43423310ecedf4eb3355
PRAVEEN KUMAR C <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-07-02 17:58:03 +0530

    Time: 17 ms (43.31%) | Memory: 47.7 MB (46.31%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -2,31 +2,25 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)return -1;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0])); 
-        int[][] dist = new int[n][n];
-        for(int i = 0 ; i < n  ; ++i){
-            Arrays.fill(dist[i],Integer.MAX_VALUE);
-        }
-        dist[0][0] = 1;
-        pq.offer(new int[]{dist[0][0],0,0});
+        Queue<int[]> q = new ArrayDeque<>(); 
+        boolean vis[][] =  new boolean[n][n];
+        q.offer(new int[]{1,0,0});
         int dirs[][] = {{0,1},{1,0},{0,-1},{-1,0},{1,1},{-1,1},{1,-1},{-1,-1}};
-        while(!pq.isEmpty()){
-            int[] cell = pq.poll();
+        vis[0][0] = true;
+        while(!q.isEmpty()){
+            int[] cell = q.poll();
             int c = cell[0];
+            if(cell[1] == n-1 && cell[2] == n-1)return c;
             for(int[] d:dirs){
                 int dx = d[0]  + cell[1];
                 int dy = d[1]  + cell[2];
-              
-                if(dx >= 0 && dx < n && dy >= 0 && dy < n && grid[dx][dy] != 1){
-                    //System.out.println(dx +" "+dy+" "+ c );
-                    if(c + 1 < dist[dx][dy] ){
-                        dist[dx][dy] = c + 1;
-                        pq.offer(new int[]{dist[dx][dy],dx,dy});
-                    }
+                if(dx >= 0 && dx < n && dy >= 0 && dy < n && grid[dx][dy] == 0 && !vis[dx][dy]){
+                        vis[dx][dy] = true;
+                        q.offer(new int[]{c+1,dx,dy});
                 }
             }
         }
-        return dist[n-1][n-1] == Integer.MAX_VALUE ? -1 : dist[n-1][n-1];
+        return -1;
         
     }
 }
