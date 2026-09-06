1152a15376eb3924474ace015a1cd991571e8ecb
PRAVEEN KUMAR C <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-09-06 15:13:34 +0530

    Time: 3 ms (97.29%) | Memory: 54.3 MB (62.97%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,15 +1,15 @@
 class Solution {
     private int f(int i,int j ,String s , String t,int[][] dp){
-        if(j == t.length())return 1;
-        if(i == s.length())return 0;
+        if(j < 0)return 1;
+        if(i < 0)return 0;
         if(j>i) return 0;
         if(dp[i][j] != -1)return dp[i][j];
         int cnt = 0;
         if(s.charAt(i) == t.charAt(j)){
-            cnt += f(i+1,j+1,s,t,dp);
-            cnt += f(i+1,j,s,t,dp);
+            cnt += f(i-1,j-1,s,t,dp);
+            cnt += f(i-1,j,s,t,dp);
         }else{
-            cnt = f(i+1,j,s,t,dp);
+            cnt = f(i-1,j,s,t,dp);
         }
         return dp[i][j] = cnt;
     }
@@ -19,7 +19,7 @@ class Solution {
         for(int i = 0 ; i < m ; ++i){
             Arrays.fill(dp[i],-1);
         }
-        return f(0,0,s,t,dp);
+        return f(m-1,n-1,s,t,dp);
 
     }
 }
