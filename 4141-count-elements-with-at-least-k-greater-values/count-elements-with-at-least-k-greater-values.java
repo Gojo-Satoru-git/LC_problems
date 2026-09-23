1f805612e81b90d82dd95ec0fbae0ecc8d8d3a74
PRAVEEN KUMAR C <160558289+Gojo-Satoru-git@users.noreply.github.com>
2026-09-23 17:56:10 +0530

    Time: 41 ms (85.96%) | Memory: 134.3 MB (40.35%) - LeetSync

--- 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.java
+++ 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.java
@@ -1,24 +1,11 @@
 class Solution {
     public int countElements(int[] nums, int k) {
+        if(nums == null) return -1;
+        if (k <= 0) return nums.length;
         Arrays.sort(nums);
-        int res = 0;
-        int n = nums.length;
-        if(k == 0)return n;
-        for(int i = 0 ; i < n - k ; ++i){
-            int num = nums[i];
-            int firstGreater = -1;
-            int low = i , high = n - 1;
-            while(low <= high){
-                int mid = low + (high - low)/2;
-                if(nums[mid] > num){
-                    firstGreater = mid;
-                    high = mid - 1;
-                }else{
-                    low = mid + 1;
-                }
-            }
-            if(firstGreater != -1 && n - firstGreater >= k)++res;
-        }
-        return res;
+        int i = nums.length - k;
+        while(i >0&& nums[i - 1] == nums[i])
+                --i;
+        return i;
     }
 }
